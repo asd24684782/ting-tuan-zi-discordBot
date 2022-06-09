0b6f0a113f39cc0f8b68275921a5d3e67ffc9ff2
asd24684782 <asd24684782@gmail.com>
2022-06-09 23:07:04 +0800

    conf token

--- src/KAMUwiki.py
+++ src/KAMUwiki.py
@@ -1,35 +1,53 @@
 # -*- coding: UTF-8 -*- 
+import logging
+from os import name
+import yamlParser
+
 import discord
+import aioredis
+from discord.ext import commands
+
 
 #client connect Discord
-client = discord.Client()
+#client = discord.Client()
+client = commands.Bot(command_prefix='.')
+#
+logging.basicConfig(level=logging.INFO)
+
+#ping
+@client.command(name = "ping")
+async def ping(ctx):
+	await ctx.channel.send("pong")
 
 #bot ready
 @client.event
 async def on_ready():
-    print('bot', client.user, 'is online')
-    game = discord.Game('動作和眼神不要太明顯')
-    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
-    await client.change_presence(status=discord.Status.idle, activity=game)
+  print('bot', client.user, 'is online')
+  game = discord.Game('動作和眼神不要太明顯')
+  #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+  await client.change_presence(status=discord.Status.idle, activity=game)
 
 #when get message
 @client.event
 async def on_message(message):
-    #confirm not myself
-    if message.author == client.user:
-        return
-    #如果以「說」開頭
-    if message.content.startswith('說'):
-      #分割訊息成兩份
-      tmp = message.content.split(" ",2)
-      #如果分割後串列長度只有1
-      if len(tmp) == 1:
-        await message.channel.send("你要我說什麼啦？")
-      else:
-        await message.channel.send(tmp[1])
+  #confirm not myself
+  if message.author == client.user:
+    return
+  #如果以「說」開頭
+  if message.content.startswith('說'):
+    #分割訊息成兩份
+    tmp = message.content.split(" ",2)
+    #如果分割後串列長度只有1
+    if len(tmp) == 1:
+      await message.channel.send("你要我說什麼啦？")
+    else:
+      await message.channel.send(tmp[1])
+  if message.content == '扶他小母牛屁眼塞炸彈':
+    await message.channel.send('牛逼屌炸天')
 
 
-    
-
-
-client.run('OTI3OTI2MTMxMDMyNzM5ODYw.YdRUjQ.qQ_eoMbpc3rshztbwxf-9MgpDls') #TOKEN在剛剛Discord Developer那邊「BOT」頁面裡面
+if __name__ == '__main__':
+  configDict = yamlParser.parser("../conf/config.yaml")
+  token = configDict['token']
+  #print(configDict['token'])
+  client.run(token) #TOKEN在剛剛Discord Developer那邊「BOT」頁面裡面
--- src/test.py
+++ src/test.py
@@ -1,36 +1,19 @@
-import asyncio 
-import time 
+# -*- coding: UTF-8 -*- 
+import redis
+import aioredis
+import asyncio
+import aiohttp
 
-async def dosomething(num): 
-    print('start{}'.format(num)) 
-    await asyncio.sleep(num) 
-    print('sleep{}'.format(num)) 
 
-async def main(): 
-    task1 = asyncio.create_task(dosomething(1)) 
-    task2 = asyncio.create_task(dosomething(2)) 
-    task3 = asyncio.create_task(dosomething(3))
-    task4 = asyncio.create_task(dosomething(4))
-    task5 = asyncio.create_task(dosomething(5))
-    task6 = asyncio.create_task(dosomething(6))
-    task7 = asyncio.create_task(dosomething(7))
-    task8 = asyncio.create_task(dosomething(8))
-    task9 = asyncio.create_task(dosomething(9))
-    
-    await task1 
-    await task2 
-    await task3
-    await task4
-    await task5
-    await task6
-    await task7
-    await task8
-    await task9
-    tasks = [dosomething(i) for i in range(1, 10)]
-    await asyncio.gather(*tasks, return_exceptions=True)
-    print(result)
+async def main():
+    r = await aioredis.from_url("redis://192.168.150.128", db=0) 
+    #print(await r.keys())
+    temp = 5
+    #print(f'the answer is {temp}')
+    print('the answer is {}'.format(temp))
+
+    #print(await r.ping())
+
 
 if __name__ == '__main__': 
-    time_start = time.time() 
     asyncio.run(main()) 
-    print(time.time() - time_start) 
